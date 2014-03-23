974a7009d9788c0fd67916016034c78e8db8cd7c
Lorenzo Fiammetta <lfiammetta@gmail.com>
2014-03-23 15:28:46 +0100

    Create bootstrap.sh

---
+++ xubuntu-android-developer/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo 'Step 1 - Update packages list...'
+echo "------------------------"
+apt-get -y update
+
+echo 'Step 2 - Install Xubuntu Desktop & co...'
+echo "------------------------"
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mousepad xubuntu-icon-theme xfce4-goodies xubuntu-wallpapers
