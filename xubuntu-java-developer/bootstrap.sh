9031a1582183d2d761a33ab940274fedc4265ef4
Lorenzo Fiammetta <lfiammetta@gmail.com>
2014-04-02 07:39:02 +0200

    Update bootstrap.sh

--- xubuntu-java-developer/bootstrap.sh
+++ xubuntu-java-developer/bootstrap.sh
@@ -11,7 +11,7 @@ apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mousepad 
 
 echo 'Step 3 - Install JDK 6 in /usr/lib/jvm/java-6-oracle...'
 echo "------------------------"
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
 sudo echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
