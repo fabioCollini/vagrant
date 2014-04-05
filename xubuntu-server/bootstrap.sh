7c31bbddff98e590c2462fdec085562ca3508180
Lorenzo Fiammetta <lfiammetta@gmail.com>
2014-04-05 10:32:05 +0200

    Update bootstrap.sh

--- xubuntu-server/bootstrap.sh
+++ xubuntu-server/bootstrap.sh
@@ -7,4 +7,4 @@ apt-get -y update
 echo 'Step 2 - Install Xubuntu Desktop & co...'
 echo "------------------------"
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mousepad xubuntu-icon-theme xfce4-goodies xubuntu-wallpapers
+apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mousepad xubuntu-icon-theme xfce4-goodies xubuntu-wallpapers gksu
