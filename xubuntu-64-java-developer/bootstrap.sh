7b2603656556251615770dbfaf7595e2589b1513
Lorenzo Fiammetta <lfiammetta@gmail.com>
2014-05-08 22:36:51 +0200

    Update bootstrap.sh

--- xubuntu-64-java-developer/bootstrap.sh
+++ xubuntu-64-java-developer/bootstrap.sh
@@ -25,3 +25,14 @@ echo "------------------------"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
 sudo dpkg -i /tmp/google-chrome*; sudo apt-get -f install -y
 rm /tmp/google*chrome*.deb
+
+echo 'Install JDK 7 in /usr/lib/jvm/java-7-oracle...'
+echo "------------------------"
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
+sudo echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get install -y oracle-jdk7-installer
+
+echo 'Install JDK 6 in /usr/lib/jvm/java-6-oracle...'
+sudo apt-get install -y oracle-java6-installer
